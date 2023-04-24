6b9516b080279b532d5320484e15c6d32688b20c
Jan Špaček <patek.mail@gmail.com>
2023-04-24 11:21:09 +0200

    Export `undefined` when WebSockets are not available

--- isomorphic-ws/web.mjs
+++ isomorphic-ws/web.mjs
@@ -1,2 +1,11 @@
-const _WebSocket = WebSocket;
+let _WebSocket;
+if (typeof WebSocket !== "undefined") {
+    _WebSocket = WebSocket;
+} else if (typeof global !== "undefined") {
+    _WebSocket = global.WebSocket;
+} else if (typeof window !== "undefined") {
+    _WebSocket = window.WebSocket;
+} else if (typeof self !== "undefined") {
+    _WebSocket = self.WebSocket;
+}
 export { _WebSocket as WebSocket };

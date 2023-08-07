3a8ac58ae612f7da52ca4c4366a7d6228814fdc3
Jan Špaček <patek.mail@gmail.com>
2023-08-07 17:13:45 +0200

    Workaround a node-fetch bug

--- isomorphic-fetch/node.cjs
+++ isomorphic-fetch/node.cjs
@@ -1,8 +1,26 @@
 "use strict";
-const fetch = require("node-fetch");
-module.exports = {
-    fetch,
-    Request: fetch.Request,
-    Response: fetch.Response,
-    Headers: fetch.Headers,
-};
+
+const nodeFetch = require("node-fetch");
+const { Request, Response, Headers } = nodeFetch;
+
+const http = require("node:http");
+const https = require("node:https");
+
+// workaround this bug: https://github.com/node-fetch/node-fetch/issues/1735
+
+const httpAgent = new http.Agent({keepAlive: true});
+const httpsAgent = new https.Agent({keepAlive: true});
+
+function agentSelector(parsedUrl) {
+    if (parsedUrl.protocol === "https:") {
+        return httpsAgent;
+    } else {
+        return httpAgent;
+    }
+}
+
+function fetch(resource, options = {}) {
+    return nodeFetch(resource, {agent: agentSelector, ...options});
+}
+
+module.exports = {fetch, Request, Response, Headers};
--- isomorphic-fetch/node.mjs
+++ isomorphic-fetch/node.mjs
@@ -1,1 +1,22 @@
-export { default as fetch, Request, Response, Headers } from 'node-fetch';
+import { default as nodeFetch } from "node-fetch";
+import http from "node:http";
+import https from "node:https";
+
+// workaround this bug: https://github.com/node-fetch/node-fetch/issues/1735
+
+const httpAgent = new http.Agent({keepAlive: true});
+const httpsAgent = new https.Agent({keepAlive: true});
+
+function agentSelector(parsedUrl) {
+    if (parsedUrl.protocol === "https:") {
+        return httpsAgent;
+    } else {
+        return httpAgent;
+    }
+}
+
+export function fetch(resource, options = {}) {
+    return nodeFetch(resource, {agent: agentSelector, ...options});
+}
+
+export { Request, Response, Headers } from 'node-fetch';
